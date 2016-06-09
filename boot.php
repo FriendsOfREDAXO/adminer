da5f2e61fcaa01cb96b44e6a8dbd501c3009ca16
Gregor Harlan <mail@gh01.de>
2016-06-09 22:48:58 +0200

    Fehlender page-Parameter besser abfangen
    fixes #2

--- boot.php
+++ boot.php
@@ -1,10 +1,12 @@
 <?php
 
-// Handle adminer redirects with missing page parameter
+// Handle adminer calls with missing page parameter
 if (
     rex::isBackend() && rex::getUser() && rex::getUser()->isAdmin() &&
-    !rex_be_controller::getCurrentPage() &&
     isset($_GET['username']) && isset($_GET['db'])
 ) {
-    rex_be_controller::setCurrentPage('adminer');
+    $page = rex_be_controller::getCurrentPage();
+    if (!$page || $page === (string) (int) $page) {
+        rex_be_controller::setCurrentPage('adminer');
+    }
 }
