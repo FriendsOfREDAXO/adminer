bfad6517bc106b0d1745178307cf7c480955e6c2
Gregor Harlan <mail@gh01.de>
2018-01-12 01:20:36 +0100

    class_exists check

--- lib/adminer.php
+++ lib/adminer.php
@@ -24,45 +24,47 @@ class rex_adminer extends Adminer
 
     function tableStructurePrint($fields)
     {
-        $table = rex_sql_table::get($_GET['table']);
-        $schema = (new rex_sql_schema_dumper())->dumpTable($table);
+        if (class_exists(rex_sql_schema_dumper::class)) {
+            $table = rex_sql_table::get($_GET['table']);
+            $schema = (new rex_sql_schema_dumper())->dumpTable($table);
 
-        // the hightlight() function needs <?php start tag
-        // for easier copy (ctrl/cmd + A) we remove the start tag from result
-        $code = "<?php \n\n".$schema;
-        $code = rex_string::highlight($code);
-        $code = str_replace('&lt;?php&nbsp;<br /><br />', '', $code);
+            // the hightlight() function needs <?php start tag
+            // for easier copy (ctrl/cmd + A) we remove the start tag from result
+            $code = "<?php \n\n".$schema;
+            $code = rex_string::highlight($code);
+            $code = str_replace('&lt;?php&nbsp;<br /><br />', '', $code);
 
-        echo '
-            <div style="margin-top: 10px;">
-                <a href="#" onclick="return !toggle(\'rex-sql-table-code\')" style="display: block">rex_sql_table code</a>
-                
-                <style type="text/css">
-                    #rex-sql-table-code {
-                        border: 1px solid #999; 
-                        background: #eee;
-                        padding: 1px 10px 5px 5px; 
-                        margin-top: 5px;
-                    }
+            echo '
+                <div style="margin-top: 10px;">
+                    <a href="#" onclick="return !toggle(\'rex-sql-table-code\')" style="display: block">rex_sql_table code</a>
                     
-                    #rex-sql-table-code pre {
-                        margin-top: 0;
-                    }
+                    <style type="text/css">
+                        #rex-sql-table-code {
+                            border: 1px solid #999; 
+                            background: #eee;
+                            padding: 1px 10px 5px 5px; 
+                            margin-top: 5px;
+                        }
+                        
+                        #rex-sql-table-code pre {
+                            margin-top: 0;
+                        }
+                        
+                        #rex-sql-table-code code {
+                            background: none;
+                        }
+                    </style>
                     
-                    #rex-sql-table-code code {
-                        background: none;
-                    }
-                </style>
-                
-                <div id="rex-sql-table-code" class="hidden" 
-                    contenteditable="true"
-                    oncut="return false"
-                    onpaste="return false"
-                    onkeydown="return event.metaKey"
-                >
-                    '.$code.'
-                </div>
-            </p>';
+                    <div id="rex-sql-table-code" class="hidden" 
+                        contenteditable="true"
+                        oncut="return false"
+                        onpaste="return false"
+                        onkeydown="return event.metaKey"
+                    >
+                        '.$code.'
+                    </div>
+                </p>';
+        }
 
         parent::tableStructurePrint($fields);
     }
