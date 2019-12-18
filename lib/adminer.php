c3dd334316068830922906abf2c6a3e242179ac5
Gregor Harlan <mail@gh01.de>
2019-12-18 23:17:10 +0100

    cs fix

--- lib/adminer.php
+++ lib/adminer.php
@@ -49,45 +49,45 @@ class rex_adminer extends Adminer
             echo '
                 <div style="margin-top: 10px;">
                     <a id="rex-sql-table-code-link" href="#" style="display: block">rex_sql_table code</a>
-                    
+
                     <style type="text/css">
                         #rex-sql-table-code {
-                            border: 1px solid #999; 
+                            border: 1px solid #999;
                             background: #eee;
-                            padding: 1px 10px 5px 5px; 
+                            padding: 1px 10px 5px 5px;
                             margin-top: 5px;
                         }
-                        
+
                         #rex-sql-table-code pre {
                             margin-top: 0;
                         }
-                        
+
                         #rex-sql-table-code code {
                             background: none;
                         }
                     </style>
-                    
+
                     <div id="rex-sql-table-code" class="hidden" contenteditable="true" spellcheck="false">
                         '.$code.'
                     </div>
-                    
+
                     <script '.nonce().'>
                         document.getElementById("rex-sql-table-code-link").addEventListener("click", function () {
                             toggle("rex-sql-table-code");
                             return false;
                         });
-                        
+
                         var code = document.getElementById("rex-sql-table-code");
-                        code.addEventListener("cut", function (event) { 
-                            event.preventDefault(); 
+                        code.addEventListener("cut", function (event) {
+                            event.preventDefault();
                         });
-                        code.addEventListener("paste", function (event) { 
-                            event.preventDefault(); 
+                        code.addEventListener("paste", function (event) {
+                            event.preventDefault();
                         });
-                        code.addEventListener("keydown", function (event) { 
+                        code.addEventListener("keydown", function (event) {
                             if (!event.metaKey) {
                                 event.preventDefault();
-                            } 
+                            }
                         });
                     </script>
                 </div>';
