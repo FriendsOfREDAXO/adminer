9881f305934dcb1c4802eb4402cf5b0ded5f0fc5
marcohanke <marcohanke@users.noreply.github.com>
2023-09-24 07:45:37 +0000

    Apply php-cs-fixer changes

--- tests/unit/name_test.php
+++ tests/unit/name_test.php
@@ -10,7 +10,7 @@ class name_test extends TestCase
     public function testName()
     {
         // test case here
-        //chose usefull assertMethod
+        // chose usefull assertMethod
         static::assertTrue(true);
     }
 }
