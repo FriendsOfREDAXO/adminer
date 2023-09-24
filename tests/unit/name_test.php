b8c26f2323e6330ea238c9461024ba6188ee0fa2
36pixel <marco@36pixel.de>
2023-09-24 09:38:11 +0200

    update scriptname

--- tests/unit/name_test.php
+++ tests/unit/name_test.php
@@ -9,6 +9,8 @@ class name_test extends TestCase
 {
     public function testName()
     {
-        // test case here + static::assertXY() method
+        // test case here
+        //chose usefull assertMethod
+        static::assertTrue(true);
     }
 }
