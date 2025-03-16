9264007b5238669f4e16ed99fa670c90d5333731
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-16 23:12:14 +0100

    Update function_adminer.php

--- functions/function_adminer.php
+++ functions/function_adminer.php
@@ -4,21 +4,9 @@ function adminer_object()
 {
     // adminer throws warning "A non-numeric value encountered" in PHP 7
     error_reporting(error_reporting() & ~E_WARNING & ~E_NOTICE);
-
+    
+    // Lade die Namespace-Funktionen
+    require_once __DIR__.'/adminer_namespace_functions.php';
+    
     return new rex_adminer();
-}
-
-// Diese Datei definiert Hilfsfunktionen für den Adminer-Namespace
-namespace Adminer {
-    function connection() {
-        return \connection();
-    }
-
-    function adminer() {
-        return \adminer();
-    }
-
-    function version() {
-        return \version();
-    }
 }
