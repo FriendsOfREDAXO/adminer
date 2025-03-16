4e2e61bf84dc0b2442672dbc18ff4577fd676620
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-16 23:12:58 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -4,6 +4,7 @@ if (true === rex::getProperty('live_mode',false))
     {
     exit;
     }
+require_once __DIR__.'/../functions/adminer_namespace_functions.php';
 require_once __DIR__.'/../functions/function_adminer.php';
 
 $databases = [];
