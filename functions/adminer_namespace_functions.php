e7219e843f5795e5407b44034c2f16e4c22b4077
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-16 23:11:53 +0100

    Create adminer_namespace_functions.php

---
+++ functions/adminer_namespace_functions.php
@@ -0,0 +1,14 @@
+<?php
+namespace Adminer;
+
+function connection() {
+    return \connection();
+}
+
+function adminer() {
+    return \adminer();
+}
+
+function version() {
+    return \version();
+}
