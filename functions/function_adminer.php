229e52cae73e6eec128546ffcbaa7c4d26cc3849
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-16 23:20:22 +0100

    Update function_adminer.php

--- functions/function_adminer.php
+++ functions/function_adminer.php
@@ -1,12 +1,10 @@
 <?php
+
 // Adminer extension, the function is called automatically by adminer
 function adminer_object()
 {
     // adminer throws warning "A non-numeric value encountered" in PHP 7
     error_reporting(error_reporting() & ~E_WARNING & ~E_NOTICE);
-    
-    // Lade die Namespace-Funktionen
-    require_once __DIR__.'/adminer_namespace_functions.php';
-    
+
     return new rex_adminer();
 }
